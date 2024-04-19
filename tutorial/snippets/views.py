2d3c87c904392756ec32dfa6558cb4708e162d20
rizvi <k214578@nu.edu.pk>
2024-04-19 09:20:26 +0500

    Add IsOwnerOrReadOnly permission to SnippetDetail view

--- tutorial/snippets/views.py
+++ tutorial/snippets/views.py
@@ -5,6 +5,7 @@ from rest_framework import generics
 from django.contrib.auth.models import User
 from snippets.serializers import UserSerializer
 from rest_framework import permissions
+from snippets.permissions import IsOwnerOrReadOnly
 
 
 class SnippetList(generics.ListCreateAPIView):
@@ -19,7 +20,8 @@ class SnippetList(generics.ListCreateAPIView):
 class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
-    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly,
+                      IsOwnerOrReadOnly]
     
     
 class UserList(generics.ListAPIView):
