67e76d3502e761e77ace1ef60af07465e046785a
rizvi <k214578@nu.edu.pk>
2024-04-20 13:27:47 +0500

    Add pagination to the API

--- tutorial/tutorial/settings.py
+++ tutorial/tutorial/settings.py
@@ -100,8 +100,11 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
-
-
+# add pagination to the API
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10
+}
 # Internationalization
 # https://docs.djangoproject.com/en/5.0/topics/i18n/
 
