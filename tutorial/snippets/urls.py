40c6f9d4be085e3e545f43dd30446fb31a9f23fe
rizvi <k214578@nu.edu.pk>
2024-04-20 13:17:34 +0500

    Add API root endpoint and SnippetHighlight view

--- tutorial/snippets/urls.py
+++ tutorial/snippets/urls.py
@@ -3,6 +3,8 @@ from rest_framework.urlpatterns import format_suffix_patterns
 from snippets import views
 
 urlpatterns = [
+    path('', views.api_root),
+    path('snippets/<int:pk>/highlight/', views.SnippetHighlight.as_view()),
     path('snippets/', views.SnippetList.as_view()),
     path('snippets/<int:pk>/', views.SnippetDetail.as_view()),
     path('users/', views.UserList.as_view()),
--- tutorial/snippets/views.py
+++ tutorial/snippets/views.py
@@ -6,6 +6,10 @@ from django.contrib.auth.models import User
 from snippets.serializers import UserSerializer
 from rest_framework import permissions
 from snippets.permissions import IsOwnerOrReadOnly
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework.reverse import reverse
+from rest_framework import renderers
 
 
 class SnippetList(generics.ListCreateAPIView):
@@ -32,6 +36,23 @@ class UserList(generics.ListAPIView):
 class UserDetail(generics.RetrieveAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
+
+@api_view(['GET'])
+def api_root(resquest, format=None):
+    return Response({
+        'users': reverse('user-list', request=resquest, format=format),
+        'snippets': reverse('snippet-list', request=resquest, format=format)
+    })
+
+class SnippetHighlight(generics.GenericAPIView):
+    queryset = Snippet.objects.all()
+    renderer_classes = [renderers.StaticHTMLRenderer]
+    
+    def get(self, request, *args, **kwargs):
+        snippet = self.get_object()
+        return Response(snippet.highlighted)
+
+
 
 
 # from snippets.models import Snippet
